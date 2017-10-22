5939debefd869be4c786b4323b3f295b2f08ffef
Paul Pukhtiy <priest1406@gmail.com>
2017-10-22 22:54:26 +0600

    Posts: new: show and index endpoints

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,20 @@
 class Api::V1::PostsController < Api::V1::BaseController
+  def index
+    page = (params[:page]).to_i
+    per_page = (params[:per_page]).to_i
+    if page == 0 || per_page == 0
+      render json: @posts = Post.all
+    else
+      render json: @posts = Post.limit(per_page).offset((page-1)*per_page)
+      response.headers['posts'] = "#{Post.count}"
+      response.headers['pages'] = "#{Post.count/per_page}"
+    end
+  end
+
+  def show
+    render json: @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
     @post.author = current_user
--- spec/api/v1/posts_spec.rb
+++ spec/api/v1/posts_spec.rb
@@ -49,4 +49,68 @@ describe Api::V1::PostsController, type: :request do
       end
     end
   end
+
+  describe :index do
+    let!(:user){ create(:user) }
+    let!(:posts){ create_list(:post, 4, author: user) }
+
+    context 'unauthorized' do
+      it 'return status 401' do
+        get api_v1_posts_path
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context 'authorized' do
+      before do
+        params = { page: 2, per_page: 2 }
+        get api_v1_posts_path, params: params, headers: auth_headers(user)
+      end
+
+      it 'return status 200' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns array of posts' do
+        expect(json_response).is_a?(Array)
+        expect(json_response.size).to eq(2)
+      end
+
+      it 'return header values' do
+        expect(response.header).to include('pages')
+        expect(response.header).to include('posts')
+      end
+    end
+  end
+
+  describe :show do
+    let!(:user){ create(:user) }
+    let!(:post){ create(:post, author: user) }
+
+    context 'authorized' do
+      before do
+        params = { id: post }
+        get api_v1_posts_path, params: params, headers: auth_headers(user)
+      end
+
+      it 'returns 200 status code' do
+        expect(response).to be_success
+      end
+
+      it 'return current post' do
+        expect(json_response).to be_present
+      end
+    end
+
+    context 'unauthorized' do
+      before do
+        params = { id: post }
+        get api_v1_posts_path, params: params
+      end
+
+      it 'return status 401' do
+        expect(response.status).to eq(401)
+      end
+    end
+  end
 end
